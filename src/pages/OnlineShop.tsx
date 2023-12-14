67537e2d734bf8276957fed91a9f7b6e6e6cd0f5
Elías <eliastrancepereyra@gmail.com>
2023-12-14 16:36:25 -0300

    better style for cart

--- src/pages/OnlineShop.tsx
+++ src/pages/OnlineShop.tsx
@@ -5,21 +5,19 @@ import "../styles/cart.css";
 function OnlineShop() {
   return (
     <div className="relative flex flex-col items-center gap-10 ">
-      <div className="absolute right-0 top-0">
-        <div>
-          <form
-            action="https://www.upland.me/"
-            method="get"
-            target="_blank"
-            className="relative flex flex-col items-center"
-          >
-            <AiOutlineShoppingCart className="cart" />
+      <div className="absolute right-0 -top-6 border">
+        <form
+          action="https://www.upland.me/"
+          method="get"
+          target="_blank"
+          className="relative flex flex-col items-center"
+        >
+          <AiOutlineShoppingCart className="cart" />
 
-            <button className="adigiana bg-[#db869b] text-black border text-sm border-black px-2 py-1 rounded-lg  hover:shadow-sm hover:shadow-black/50 duration-500">
-              TIENDA VIRTUAL
-            </button>
-          </form>
-        </div>
+          <button className="adigiana bg-[#db869b] text-black border text-sm border-black px-2 py-1 rounded-lg  hover:shadow-sm hover:shadow-black/50 duration-500">
+            TIENDA VIRTUAL
+          </button>
+        </form>
       </div>
 
       <div className="my-20">
